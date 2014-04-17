300e058a53302b97733dfad346dcd0f365f2edd3
jjennings955 <jjennings955@gmail.com>
2014-04-16 20:26:58 -0500

    Merged Schyler's Changes
    Merged Schyler's Changes to Medicine and Vital Fragments

--- src/com/team4/healthmonitor/MedicineFragment.java
+++ src/com/team4/healthmonitor/MedicineFragment.java
@@ -1,16 +1,15 @@
 package com.team4.healthmonitor;
 
 
-
-import java.util.ArrayList;
-
+import java.util.Date;
+import java.util.List;
 
 import com.team4.database.DatabaseHandler;
-import com.team4.database.User;
+import com.team4.database.Medication;
+import com.team4.database.VitalSign;
 import com.team4.healthmonitor.R;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -21,8 +20,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TableLayout;
-import android.widget.TableRow;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,14 +29,6 @@ public class MedicineFragment extends Fragment
 {
 
 	private FragmentActivity myContext;
-
-	private String key = "username";
-
-	private String username;
-	private String password;
-
-	
-	
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) 
@@ -47,74 +37,50 @@ public class MedicineFragment extends Fragment
 		View rootView = inflater.inflate(R.layout.fragment_medicine, container, false);
 		setHasOptionsMenu(true);
 		//((MainAppActivity) getActivity()).setActionBarTitle("Medicine");
+    
+		LinearLayout l1 = (LinearLayout)rootView.findViewById(R.id.fragment_med);
 		
-		
-	/*
-		Bundle bundle=getArguments(); 
-		String username = bundle.getString("username");
-		*/
-		
-		/*	
-		Bundle bundle = this.getArguments();
-		String username = bundle.getString(key,"username");
-	*/	
-		
-		
-		Intent i = getActivity().getIntent();
-		username = i.getStringExtra(MainActivity.USERNAME);
-		password = i.getStringExtra(MainActivity.PASSWORD);
-		
-		DatabaseHandler db = new DatabaseHandler(getActivity());
-		User u = db.login(username, password);
-		
-		String f = u.getFirstName();
-		String l = u.getLastName();
-		int age = u.getAge();
-		char g = u.getGender();
-		
-		
-		
-		//Toast.makeText(getActivity(), username +" "+password, Toast.LENGTH_SHORT).show();
-		//Toast.makeText(getActivity(), myContext+"", Toast.LENGTH_SHORT).show();
-		
+		TextView[] tv = new TextView[4];
+	       
+	       LinearLayout l =null;
+	       DatabaseHandler db = new DatabaseHandler(getActivity());
+	       List<Medication> vt = db.getMedications();       
+		     for (Medication cn1 : vt) {
+		    	  for(int j1=0; j1<4; j1++){
+		    		   l = new LinearLayout(getActivity().getBaseContext());
+		    		   l.setLayoutParams(new ViewGroup.LayoutParams(
+		    			        ViewGroup.LayoutParams.MATCH_PARENT,
+		    			        ViewGroup.LayoutParams.WRAP_CONTENT));
+		    		   //l.setBackgroundResource(R.id.myshape_s);
 
-   
-		
-		
-	    TableLayout ll = (TableLayout) rootView.findViewById(R.id.tableLayout);
-
-
-	    User dummy = new User();
-	    dummy.setId(1);
-	    ArrayList<String[]> results = db.getUserMedicationSchedule(dummy);
-	    int j = 0;
-	    for (String[] resultRow : results) 
-
-	    {
-
-	        TableRow row= new TableRow(getActivity());
-	        TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
-	        row.setLayoutParams(lp);
-
-	        TextView a = new TextView(getActivity());
-	        TextView b = new TextView(getActivity());
-	        TextView c = new TextView(getActivity());
-	        TextView d = new TextView(getActivity());
-	        a.setText(resultRow[0]);
-	        b.setText(resultRow[1]);
-	        c.setText(resultRow[2]);
-	        d.setText(resultRow[3]);
-	        row.addView(a);
-	        row.addView(b);
-	        row.addView(c);
-	        row.addView(d);
-	        ll.addView(row,j++);
-	    }
-
-
+		    		  tv[j1] = new TextView(getActivity());
+		    		  tv[j1].setLayoutParams(new ViewGroup.LayoutParams(
+		    			        ViewGroup.LayoutParams.WRAP_CONTENT,
+		    			        ViewGroup.LayoutParams.WRAP_CONTENT));
+		    		  tv[j1].setWidth(150);
+		    		  //tv[j1].setText("Dynamic Text!");
+		    	  	 //l.addView(tv[j1]);
+		    	  }
+		    	  Date d = new Date();
+		    	  tv[0].setText(""+cn1.getId());
+		    	  tv[1].setText(""+cn1.getName());
+		    	  tv[2].setText(""+cn1.getPriority());
+		    	  tv[3].setText(""+d.getMonth()+"/"+d.getDay()+"/"+d.getYear());
+		          
+		    	  for(int j1=0; j1<4; j1++){
+		    		 // tv[j1] = new TextView(container.getContext());
+		    	  	  l.addView(tv[j1]);
+		    	  }
+		    	  l1.addView(l);
+		          }
 		
 		return rootView;
+		//return l;
 	}
+		
+		
+		
+	
 	
 	@Override
 	public void onAttach(Activity activity)
@@ -128,7 +94,35 @@ public class MedicineFragment extends Fragment
 	{
 	   inflater.inflate(R.menu.medicine_menu, menu);
 	   
-
+		/*	add = (Button) getView().findViewById(R.id.add_item_medicine);
+	   
+		// add button listener
+				add.setOnClickListener(new OnClickListener() {
+		 
+				  @Override
+				  public void onClick(View arg0) {
+		 
+					// custom dialog
+					final Dialog dialog = new Dialog(context);
+					dialog.setContentView(R.layout.dialog_medicine);
+					dialog.setTitle("Title...");
+		 
+					// set the custom dialog components - text, image and button
+					TextView text = (TextView) dialog.findViewById(R.id.text);
+					text.setText("Android custom dialog example!");
+					Button dialogButton = (Button) dialog.findViewById(R.id.dialogButtonOK);
+					// if button is clicked, close the custom dialog
+					dialogButton.setOnClickListener(new OnClickListener() {
+						@Override
+						public void onClick(View v) {
+							dialog.dismiss();
+						}
+					});
+		 
+					dialog.show();
+				  }
+				});*/
+	   
 	   
 	}
 	
@@ -142,7 +136,7 @@ public class MedicineFragment extends Fragment
 	        	showMedicineDialog();
 	            return true;
 	        case R.id.settings_item:
-	        	Toast.makeText(getActivity(), "Settings",
+	        	Toast.makeText(getActivity(), "Search",
 	        		      Toast.LENGTH_SHORT).show();
 	        	return true;
 	        default:
