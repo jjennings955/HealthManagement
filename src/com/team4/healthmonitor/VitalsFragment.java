c2a300a23d78c3837c71a47a846b913e90ca0f3f
devfirejedi4 <devkishen.sisodia@gmail.com>
2014-04-21 10:58:24 -0500

    commit to switch branches
    dialog_vitals, vitalsDialog, vitalsFrag,

--- src/com/team4/healthmonitor/VitalsFragment.java
+++ src/com/team4/healthmonitor/VitalsFragment.java
@@ -1,16 +1,24 @@
 package com.team4.healthmonitor;
 
 
+import java.util.ArrayList;
+
 import com.team4.healthmonitor.R;
+
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.Toast;
+ 
 
 public class VitalsFragment extends Fragment 
 {
@@ -19,6 +27,7 @@ public class VitalsFragment extends Fragment
 	private String username;
 	private String password;
 
+	private FragmentActivity myContext2;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
@@ -26,11 +35,20 @@ public class VitalsFragment extends Fragment
 		View rootView = inflater.inflate(R.layout.fragment_vitals, container, false);
 		setHasOptionsMenu(true);
 		
+		
+		
 		//((MainAppActivity) getActivity()).setActionBarTitle("Vitals");
 		
 		//Toast.makeText(getActivity(), myContext+"", Toast.LENGTH_SHORT).show();
 		
 		return rootView;
+		
+	}
+	
+	public void onAttach(Activity activity)
+	{
+	    myContext2=(FragmentActivity) activity;
+	    super.onAttach(activity);
 	}
 	
 	
@@ -38,5 +56,30 @@ public class VitalsFragment extends Fragment
 	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) 
 	{
 	   inflater.inflate(R.menu.vitals_menu, menu);
+
 	}
+	
+	public boolean onOptionsItemSelected(MenuItem item) 
+	{
+	    // Handle item selection
+	    switch (item.getItemId()) 
+	    {
+	        case R.id.add_item_vitals:
+	        	showVitalDialog();
+	            return true;
+	        case R.id.settings_item:
+	        	Toast.makeText(getActivity(), "Search",
+	        		      Toast.LENGTH_SHORT).show();
+	        	return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
+	
+    private void showVitalDialog()
+    {
+        FragmentManager fm2 = myContext2.getSupportFragmentManager();
+        VitalDialog vd = new VitalDialog();
+        vd.show(fm2, "fragment_edit_name");
+    }
 }
